c845540721d34460ce145e8ae30a47ecdb41f3a5
Alexandr Vlasenko <olexandr.vlasenko@gmail.com>
2017-07-20 23:21:41 +0300

    translating

--- src/js/main.js
+++ src/js/main.js
@@ -361,47 +361,49 @@ function getTargetDate(Milliseconds_to_count){
     var days, hours, minutes, seconds;
     // get tag element
     var countdown = document.getElementById("pay-timer");
-        // var d = document.getElementById("day");
-        // var h = document.getElementById("hour");
-        var m = document.getElementById("minute");
-        var s = document.getElementById("second");
-        // find the amount of "seconds" between now and target
-        var current_date = new Date().getTime();
-        var seconds_left = (target_date - current_date);
-        localStorage.setItem("Milliseconds", JSON.stringify(seconds_left));
-        if(seconds_left < 1000){
-            target_date = getTargetDate(Milliseconds_to_count);
-            time = 1000;
+        if(countdown) {
+            // var d = document.getElementById("day");
+            // var h = document.getElementById("hour");
+            var m = document.getElementById("minute");
+            var s = document.getElementById("second");
+            // find the amount of "seconds" between now and target
+            var current_date = new Date().getTime();
+            var seconds_left = (target_date - current_date);
+            localStorage.setItem("Milliseconds", JSON.stringify(seconds_left));
+            if(seconds_left < 1000){
+                target_date = getTargetDate(Milliseconds_to_count);
+                time = 1000;
+            }
+            seconds_left /= 1000;
+            // do some time calculations
+            // days = parseInt(seconds_left / 86400);
+            // seconds_left = seconds_left % 86400;
+        
+            // hours = parseInt(seconds_left / 3600);
+            // seconds_left = seconds_left % 3600;
+        
+            minutes = parseInt(seconds_left / 60);
+            seconds = parseInt(seconds_left % 60);
+        
+            // if(days < 10){
+            //     days = "0" + days;
+            // }
+            // if(hours < 10){
+            //     hours = "0" + hours;
+            // }
+            if(minutes < 10){
+                minutes = "0" + minutes;
+            }
+            if(seconds < 10){
+                seconds = "0" + seconds;
+            }
+        
+            // d.innerHTML = days;
+            // h.innerHTML = hours;
+            m.innerHTML = minutes;
+            s.innerHTML = seconds;
+            window.setTimeout(timer, time)
         }
-        seconds_left /= 1000;
-        // do some time calculations
-        // days = parseInt(seconds_left / 86400);
-        // seconds_left = seconds_left % 86400;
-     
-        // hours = parseInt(seconds_left / 3600);
-        // seconds_left = seconds_left % 3600;
-     
-        minutes = parseInt(seconds_left / 60);
-        seconds = parseInt(seconds_left % 60);
-     
-        // if(days < 10){
-        //     days = "0" + days;
-        // }
-        // if(hours < 10){
-        //     hours = "0" + hours;
-        // }
-        if(minutes < 10){
-            minutes = "0" + minutes;
-        }
-        if(seconds < 10){
-            seconds = "0" + seconds;
-        }
-     
-        // d.innerHTML = days;
-        // h.innerHTML = hours;
-        m.innerHTML = minutes;
-        s.innerHTML = seconds;
-        window.setTimeout(timer, time)
     };
 timer()
 
